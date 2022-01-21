9363270bacf2586f65bff4239260bd94d086db1b
ellabellla <86757313+ellabellla@users.noreply.github.com>
2022-01-21 15:45:17 +1100

    + comments and refactor imports

--- i3-keys.py
+++ i3-keys.py
@@ -1,10 +1,19 @@
-import argparse
-import os
-import re
-import markdown
-import webbrowser
+"""
+    i3-keys
+    
+    A program to scape the i3 config and display the keybinds in an easy to read format.
+    
+    Author: Ella Pash
+"""
+import argparse, os, re, markdown, webbrowser
 
 def parse_args():
+    """parse command line arguments and return an input data and an output stream
+
+    Returns:
+        file: file contents
+        out: out stream (either stdout or a file)
+    """
     parser = argparse.ArgumentParser('i3Key Scaper')
     parser.add_argument('-c', '--config', type=argparse.FileType('r'), default=os.path.expanduser('~/.i3/config'))
     parser.add_argument('-o','--out', type=argparse.FileType('w'), default='-')
@@ -19,19 +28,45 @@ def parse_args():
     return args
 
 def extract_keybinds(contents):
+    """extract each coomented keybind block to an array
+
+    Args:
+        contents (string): contents of the i3 config
+
+    Returns:
+        array: array of tuples containing the matched commented keybind block
+    """
     return re.findall(r'((#(((?!bindsym)(?!exec).)*?)\n)+(#?\s*bindsym\s\$(mod|alt)(.*?)\s(.*?)\n)+)', contents)
 
 prettify_extract_comments = re.compile(r'#\s*(((?!bindsym).)*?)\r?\n')
 prettify_extract_shortcut = re.compile(r'(#?)\s*bindsym\s\$(mod|alt)(.*?)\s(.*)')
 def prettify_keybind(keybind):
+    """extract each part of a keybind block and format it into markdown
+
+    Args:
+        keybind (string): keybind block
+
+    Returns:
+        string: formatted keybind block
+    """
     comments = '\n'.join([ '## ' + comment[0] for comment in prettify_extract_comments.findall(keybind)]);
     shortcut = '\n'.join([f'- {shortcut[1] +shortcut[2]} -> {shortcut[3]} {( "(not in use)" if shortcut[0] == "#" else "")}' for shortcut in prettify_extract_shortcut.findall(keybind)])
     return f'{comments}:\n{shortcut}';
 
 def process_config(file_contents):
+    """extract all keybind blocks and format them into markdown
+
+    Args:
+        file_contents (string): i3 config contents
+
+    Returns:
+        string: formatted keybind list
+    """
     return '\n\n'.join([prettify_keybind(key[0]) for key in extract_keybinds(file_contents)])
     
 def main():
+    """generate formatted list of keybind blocks from the i3 config, convert to html then open in a browser
+    """
     args = parse_args()
     
     html = markdown.markdown('# i3 Keybinds\n'+process_config(args.config))
@@ -41,10 +76,7 @@ def main():
     if args.out.name != '<stdout>':
         webbrowser.open(args.out.name)
     
-    
-    
-    
-    
+
 
 
 if __name__ == "__main__":
